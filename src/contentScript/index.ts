1f10689318fe088214694f80706f20c46837d2d5
bwat47 <bwat47@gmail.com>
2026-07-06 18:58:42 -0400

    refactor: standard export default function(...)

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -5,14 +5,12 @@ import type { CodeMirrorControl } from 'api/types';
 import codeMirror6Plugin from './codeMirror6Plugin';
 import type { PluginContext } from './types';
 
-module.exports = {
-    default: function (context: PluginContext) {
-        return {
-            plugin: (CodeMirror: CodeMirrorControl) => {
-                if (CodeMirror.cm6) {
-                    codeMirror6Plugin(context, CodeMirror);
-                }
-            },
-        };
-    },
-};
+export default function (context: PluginContext) {
+    return {
+        plugin: (CodeMirror: CodeMirrorControl) => {
+            if (CodeMirror.cm6) {
+                codeMirror6Plugin(context, CodeMirror);
+            }
+        },
+    };
+}
